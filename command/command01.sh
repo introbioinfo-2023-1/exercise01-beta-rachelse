75a3708817524e57a4b72bc9a678567c10f1bc76
rachelse <seamustard52@gmail.com>
2023-04-12 18:15:17 +0900

    changed id

--- command/command01.sh
+++ command/command01.sh
@@ -1,7 +1,7 @@
 # 1. Print your student number and github ID and copy the printed result to "student_id.txt".
 # The format should be like "2021-#####,harryporter" (student number-github ID).
 
-echo "2023-23098, rachelse" > ./result/student_id.txt
+echo "2023-23000,rachelse" > ./result/student_id.txt
 # 2. Download the fasta file with NCBI accession. The downloaded file should be named like "NC_XXXXXX.X.fasta" (No result file)
 # - Sequence name: Saccharomyces cerevisiae S288C chromosome I, complete sequence
 # - NCBI accession number: NC_001133.9
